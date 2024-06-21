4f7d73799f3496f093aa0bb755c82147185351ce
Kallie Owens <queenkallie12@gmail.com>
2024-06-21 13:34:22 -0400

    still working

--- AboutMe/ContentView.swift
+++ AboutMe/ContentView.swift
@@ -9,12 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack(spacing: 20) {
+            Image("fam(1)")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+            Image("animals(2)")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
         }
+    
+        .padding()
+        
+        HStack(){
+            Text("Kallie Owens")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(Color.blue)
+        }
+        
+        VStack(){
+            Text("Click the pictures to learn more!")
+                .font(.headline)
+                .fontWeight(.semibold)
+                .foregroundColor(Color.blue)
+
+        }
+        
+        HStack(spacing: 20){
+            Image("theatre(3)")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+            Image("coding(4)")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+       
+        }
+        
         .padding()
     }
 }
